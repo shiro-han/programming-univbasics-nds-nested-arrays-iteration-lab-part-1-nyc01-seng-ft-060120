c3309f17043d559f7f8c24a3702fd21768b4f032
Shiro Han <shan99business@gmail.com>
2020-05-22 19:11:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,8 +3,8 @@ def find_even_values(src)
   #
   # Output all even values in each nested array
   index1 = 0
-  index2 = 0
   while index1 < src.count do
+    index2 = 0
     while index2 < src[index1].count do
       index2 += 1
       current_value = src[index1][index2].to_i
